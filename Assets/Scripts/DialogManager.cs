0d1c9699b1f07ab151dd224c527959e9e52403d6
Haruna Kaze <haruna.kaze@gmail.com>
2016-02-01 07:24:13 +0700

    - Toggle Victory

---
+++ Assets/Scripts/DialogManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DialogManager : MonoBehaviour {
+
+	public GameObject dialog;
+	public GameObject showButton;
+
+	public void ToggleDialogMessage() {
+		dialog.SetActive (!dialog.activeInHierarchy);
+		showButton.SetActive(!showButton.activeInHierarchy);
+	}
+}
